174b5a3431ded14a6659714e32ebe37d7baf5146
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-07-15 17:24:46 +0545

    Added enviromental variables

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 
 const app = express();
 
-PORT = 5000;
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
 	console.log(`Server started on ${PORT}`);
