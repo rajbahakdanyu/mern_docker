155752be768f620a79b7226bd679caf019414c53
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-07-19 19:27:30 +0545

    Added update task

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -36,3 +36,26 @@ exports.addTask = async (req, res, next) => {
         })
     }
 }
+
+exports.updateTask = async (req, res, next) => {
+    try {
+        req.body["completed"] = !req.body["completed"]
+
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        })
+
+        res.status(200).json({
+            status: "success",
+            data: {
+                task,
+            },
+        })
+    } catch (e) {
+        console.log(e)
+        res.send(400).json({
+            status: "error",
+        })
+    }
+}
--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -6,4 +6,6 @@ const router = express.Router()
 
 router.route("/").get(taskController.getTasks).post(taskController.addTask)
 
+router.route("/:id").patch(taskController.updateTask)
+
 module.exports = router
