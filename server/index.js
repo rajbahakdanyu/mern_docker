5ab14a104d5fe99a9db7611745cd9a418c070a66
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-07-19 17:40:13 +0545

    Added routes to app

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 
+const taskRouter = require('./routes/taskRoutes')
+
 const app = express();
 
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/api/v1/", (req, res) => {
+  res.send("Its Alive!!!");
 });
+
+app.use("/api/v1/tasks", taskRouter);
 
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
