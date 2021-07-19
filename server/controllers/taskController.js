3ca0f8352c2ebd0a978987bbcb0d1d2fdad225a2
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-07-19 18:45:53 +0545

    Added get tasks controller

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,1 +1,20 @@
 const Task = require('../models/taskModel')
+
+exports.getTasks = async (req,ress,next) => {
+    try{
+        const tasks = await Task.find()
+
+        res.status(200).json({
+            status: "success",
+            results: tasks.length,
+            data: {
+                tasks,
+            },
+        })
+    } catch(e) {
+        console.log(e)
+        res.status(400).json({
+            status: "error",
+        })
+    }
+}
