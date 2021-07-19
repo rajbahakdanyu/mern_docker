f4f593ec361a87456199d20370b2b587b1baad7e
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-07-19 17:54:30 +0545

    Added mongoDB to app

--- server/index.js
+++ server/index.js
@@ -1,8 +1,27 @@
 const express = require("express");
+const mongoose = require('mongoose')
 
 const taskRouter = require('./routes/taskRoutes')
 
 const app = express();
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+const connectWithRetry = () => {
+	mongoose
+		.connect(mongoURL, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useFindAndModify: false,
+		})
+		.then(() => console.log("Successfully connected to DB"))
+		.catch((e) => {
+			console.log(e);
+			setTimeout(connectWithRetry, 5000);
+		});
+};
+
+connectWithRetry();
 
 app.use(express.json());
 
