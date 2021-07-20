f590824214f9fad721ea451db7dd9ead27d6c765
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-07-20 14:07:15 +0545

    Added add button

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,12 @@
 import React from "react"
 
+import Button from "./Button"
+
 const Header = () => {
     return (
         <header className='header'>
             <h1>Todo</h1>
+            <Button />
         </header>
     )
 }
