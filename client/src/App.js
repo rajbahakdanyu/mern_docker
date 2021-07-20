17c7f60cebf1232c3d68844037608437bf55d4fa
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-07-20 14:40:30 +0545

    Added fetch tasks method

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
+import { useState, useEffect } from "react"
+
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
 
 function App() {
+    const [tasks, setTasks] = useState([])
+
+    // Fetch tasks
+    const fetchTasks = async () => {
+        const res = await fetch("http://localhost:5000/api/v1/", {
+            method: "GET",
+        })
+        const data = await res.json()
+
+        return data
+    }
+
     return (
         <div className='container'>
             <Header />
